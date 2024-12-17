064d0e4ce0accf42e97c655d03f4988247a55fdb
Cesare <124678858+Cesare94@users.noreply.github.com>
2024-12-17 11:47:22 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('#iconMenu').click(function(){
+        $('nav ul').show();
+        
+       
+
+    })
+    $('#iconClose').click(function() {
+        $('nav ul').hide();
+        
+    })
+
+})
+    
+
